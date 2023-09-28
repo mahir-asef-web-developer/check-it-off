bdb141a4ca01e4f69441d3a8db0c165fa66de300
Md.Mahir Asef <programmermahirasef@yahoo.com>
2023-09-28 13:30:10 +0600

    Logger has been completed

--- server/middlewares/logger.middleware.js
+++ server/middlewares/logger.middleware.js
@@ -1,7 +1,19 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const logger = (req, res, next) => {
+  const { authorization } = req.header;
+  var user;
+  if (authorization) {
+    const token = authorization.split(" ")[1];
+    const decode = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+    user = decode.uuid;
+  }
   console.log("");
   console.log(
-    `${req.hostname}-${req.protocol}-${req.method}-${req.originalUrl}`
+    `start-[${new Date()}]-[${req.hostname}]-[${req.ip}]-[${user}]-[${
+      req.protocol
+    }]-[${req.method}]-[${req.originalUrl}]-end`
   );
   next();
 };
